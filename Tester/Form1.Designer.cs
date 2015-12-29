e87abfae3022f8b084ee3f2ace33d01cc8f62e07
scott.london <scott.london@carefusion.com>
2015-12-29 11:09:30 -0800

    First Commit

---
+++ Tester/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connect = new System.Windows.Forms.LinkLabel();
+            this.connectStatus = new System.Windows.Forms.Label();
+            this.oAuthBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // connect
+            // 
+            this.connect.AutoSize = true;
+            this.connect.Location = new System.Drawing.Point(753, 9);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(47, 13);
+            this.connect.TabIndex = 0;
+            this.connect.TabStop = true;
+            this.connect.Text = "Connect";
+            this.connect.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.connect_LinkClicked);
+            // 
+            // connectStatus
+            // 
+            this.connectStatus.AutoSize = true;
+            this.connectStatus.Location = new System.Drawing.Point(12, 9);
+            this.connectStatus.Name = "connectStatus";
+            this.connectStatus.Size = new System.Drawing.Size(73, 13);
+            this.connectStatus.TabIndex = 1;
+            this.connectStatus.Text = "Disconnected";
+            // 
+            // oAuthBrowser
+            // 
+            this.oAuthBrowser.Location = new System.Drawing.Point(15, 32);
+            this.oAuthBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.oAuthBrowser.Name = "oAuthBrowser";
+            this.oAuthBrowser.Size = new System.Drawing.Size(785, 401);
+            this.oAuthBrowser.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 455);
+            this.Controls.Add(this.oAuthBrowser);
+            this.Controls.Add(this.connectStatus);
+            this.Controls.Add(this.connect);
+            this.Name = "Form1";
+            this.Text = "Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel connect;
+        private System.Windows.Forms.Label connectStatus;
+        private System.Windows.Forms.WebBrowser oAuthBrowser;
+    }
+}
+
---
+++ Tester/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Json;
+using System.Net.Http;
+using System.Windows.Forms;
+
+using Octokit;
+
+namespace Tester
+{
+    public partial class Form1 : Form
+    {
+        //github-rd.carefusion.com
+        //const string CLIENT_ID = "cbba4fab68793ec8390b";
+        //const string CLIENT_SECRET = "b09adb2cfe8c044c5f3d766ea8168ee008b4fac1";
+
+        //github.com
+        const string CLIENT_ID = "6ffd31e5f9c828b1087d";
+        const string CLIENT_SECRET = "119e5b0efc467325be1e62e84176b96df28c6ff6";
+
+        const string APP_NAME = "Tester";
+        const string GITHUB_URI = "https://github.com";
+
+        //TESTING
+        static string address = "http://api.worldbank.org/countries?format=json";
+
+        bool connected = false;
+        string accessToken = "";
+        static Uri gitUri = new Uri(GITHUB_URI);
+        GitHubClient client = new GitHubClient(new ProductHeaderValue(APP_NAME), gitUri);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void connect_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (!connected)
+                Connect();
+            else
+                Disconnect();
+        }
+
+        private async void Connect()
+        {
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                client.Credentials = new Credentials(accessToken);
+            }
+
+            try
+            {
+                var repostiories = await client.Repository.GetAllForCurrent();
+                var brake = 0;
+            }
+            catch (AuthorizationException)
+            {
+                //make a request to the authorization page
+                var url = GetOAuthLogin();
+                var navi = System.Net.WebUtility.UrlDecode(url.OriginalString);
+                oAuthBrowser.Navigate(navi);
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.ToString(), "FUCK", MessageBoxButtons.OK);
+            }
+
+            #region Test Request
+            /************TEST REQUEST**********************************************/
+            /*//create a web request.  You will need to look at how crawlr uses REST for octo deploy
+            var request = new HttpClient();
+            
+
+            HttpResponseMessage response = await request.GetAsync(GITHUB_URI);
+            
+            response.EnsureSuccessStatusCode();
+
+            var contents = await response.Content.ReadAsStreamAsync();
+
+
+            //var array = JsonArray.Load(contents);
+            //for (int i = 0; i < array[1].Count; i++)
+            //{
+            //    string id = array[1][i]["id"].ToString();
+            //    string name = array[1][i]["name"].ToString();
+            //}
+            
+            */
+            #endregion
+
+
+            connectStatus.Text = "Connected";
+            connected = true;
+        }
+
+        private Uri GetOAuthLogin()
+        {
+            var csrf = System.Web.Security.Membership.GeneratePassword(24, 1);
+            var request = new OauthLoginRequest(CLIENT_ID)
+            {
+                
+            };
+            var uri = client.Oauth.GetGitHubLoginUrl(request);
+            return uri;
+        }
+
+        private void Disconnect()
+        {
+            connectStatus.Text = "Disconneted";
+            connected = false;
+        }
+    }
+}
